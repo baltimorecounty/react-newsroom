4efa6982b977163375f6f9c03b1eb25202d5d7bc
sgurung <sgurung@OIT228469>
2020-02-10 10:25:16 -0500

    format

--- src/components/NewsRoomCard.jsx
+++ src/components/NewsRoomCard.jsx
@@ -23,24 +23,23 @@ const NewsRoomCard = props => {
   const { Label, Value } = Category;
 
   return (
- <div className="dg_card1">
-         <div className="itemOne"> <h3>{ArticleSummary}</h3></div>
-          <div className="itemTwo defaultWidth">
+ <div className="newsRoomCard">
+         <div className="itemHeader"> <h3>{ArticleSummary}</h3></div>
+          <div className="itemMiddle itemMiddleWidth">
            <div id="dg_main-content">
             <img
             className="dg_image left"
-              //className="imageSize"
               src="//baltimorecountymd.gov/sebin/t/t/homepage-county-executive.jpg"
               alt="County Executive Olzsewski delivers a speech."
             />
              </div> 
           </div>
-          <div className="itemTwo "> <div className="row"> <a href={Url}>{PublishDate}</a>
+          <div className="itemMiddle "> <div className="itemLink"> <a href={Url}>{PublishDate} </a>  
             <a href={Url}>{Author}</a></div>
             <p>{ArticleSummary}</p>
             </div> 
        
-            <div className="itemOne">
+            <div className="itemFooter">
             <div class="row justify-content-end">
           <div class="col-lg-2 col-md-2 col-sm-2">
             <Button text="Details" />
