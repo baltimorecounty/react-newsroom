6b21816cbba68519848d6ddfed0afd2231ff1f5f
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-02-13 15:20:58 -0500

    Fixed formating

--- src/components/NewsRoomCard.jsx
+++ src/components/NewsRoomCard.jsx
@@ -31,51 +31,22 @@ const NewsRoomCard = props => {
     <i className="far fa-newspaper fa-3x dg_news_icon"></i>
   );
 
-  const dateSection = (
-    <div className="dg_card_date-author_separator">
-      <p>{published}</p>
-    </div>
-  );
-
-  const authorSection = (
-    <div>
-      <p>{author}</p>
-    </div>
-  );
-
-  const newsSummary = <p>{articleSummary}</p>;
-
   return (
     <Card className="text-left">
+      <h2>{title}</h2>
       <CardContent>
-        <div className="contanier">
-          <div className="row">
-            <h3>{title}</h3>
-          </div>
-          <div className="row">
-            <div className="col-lg-3 col-md-6 col-sm-9 col-xs-12">
-              {imageSection}
-            </div>
-            <div className="col-lg-9 col-md-12">
-              <div className="row">
-                <div className="col-lg-2 col-md-4 col-xs-2">{dateSection}</div>
-                <div className="col-lg-10 col-md-8 col-xs-2">
-                  {authorSection}
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-12">{newsSummary}</div>
-              </div>
-            </div>
+        <div className="row">
+          <div className="col-md-3 col-xs-12">{imageSection}</div>
+          <div className="col-md-9 col-xs-12">
+            <p>
+              <span>{published}</span> | <span>{author}</span>
+            </p>
+            <p>{articleSummary}</p>
           </div>
         </div>
       </CardContent>
-      <CardFooter>
-        <div className="row offset-lg-10 offset-md-8 offset-xs-6">
-          <div className="col-2">
-            <Button as="a" text="Details" href={url} />
-          </div>
-        </div>
+      <CardFooter className="text-right">
+        <Button as="a" text="Details" href={url} />
       </CardFooter>
     </Card>
   );
