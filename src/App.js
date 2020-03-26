6dcd4abdc1b2d2cfc39809e306e84106f8f7715d
martypowell <jpowel25@gmail.com>
2020-03-26 10:07:07 -0400

    added filter list from npm

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import "@baltimorecounty/dotgov-components/lib/styles/dotgov.min.css";
 import "./App.css";
 
 import { Config } from "@baltimorecounty/javascript-utilities";
-import React from "react";
+import { FilterList } from "@baltimorecounty/react-filter-list";
 import NewsRoomList from "./components/NewsRoomList";
+import React from "react";
 
-const { setConfig } = Config;
+const { setConfig, getValue } = Config;
 
-const testApiRoot = "https://testservices.baltimorecountymd.gov/api";
-const prodApiRoot = "https://services.baltimorecountymd.gov/api";
+const testApiRoot = "https://testservices.baltimorecountymd.gov/hub/api/news";
+const prodApiRoot = "https://services.baltimorecountymd.gov/hub/api/news";
 
 // HACK - the Config utiltiy does not account for beta.
 // TODO: This will need to be addressed when we get closer to launch
 const localApiRoot =
   window.location.hostname.indexOf("beta") > -1
     ? testApiRoot
-    : "//localhost:54727/api";
+    : "https://localhost:44393/api/news";
 
 const configValues = {
   local: {
@@ -34,8 +35,37 @@ const configValues = {
 
 setConfig(configValues);
 
+const filters = [
+  {
+    targetApiField: "category.value",
+    displayName: "Category",
+    options: [
+      { value: "releases", label: "News Releases" },
+      { value: "stories", label: "Stories" }
+    ]
+  }
+];
+
 function App() {
-  return <NewsRoomList />;
+  return (
+    <FilterList
+      title="Baltimore County Newsroom"
+      filters={filters}
+      apiEndpoint={getValue("apiRoot")}
+      renderItem={({ title, articleSummary }) => (
+        <div
+          style={{
+            border: "1px solid #e0e0e0",
+            padding: "10px",
+            marginBottom: "10px"
+          }}
+        >
+          <h2>{title}</h2>
+          <p>{articleSummary}</p>
+        </div>
+      )}
+    />
+  );
 }
 
 export default App;
